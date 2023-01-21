4c97c4ef4be99b2cc1c88872276934ba45cbf1c9
kostinos <48163283+kostinos@users.noreply.github.com>
2023-01-21 22:06:17 +0300

    my settings 21012023

---
+++ requirements.sh
@@ -0,0 +1,6 @@
+#Use ZSH
+chsh -s /bin/zsh
+# Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
