7bec9d26e88784186b02c4c59b82f6056e12c940
kostinos <48163283+kostinos@users.noreply.github.com>
2023-01-21 22:30:51 +0300

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,7 @@
 #Use ZSH
 chsh -s /bin/zsh
 # Homebrew
+xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 #oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
