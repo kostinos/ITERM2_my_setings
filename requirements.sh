aaa64a034b48fc1f34f5df3d54aa5422813ea596
kostinos <48163283+kostinos@users.noreply.github.com>
2025-11-10 18:56:47 +0300

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,21 +1,8 @@
-SPCTLisDisabled=1
-CURRENT_USER=$(stat -f '%Su' /dev/console)
-
-
-if [[ $(whoami) != "root" ]]; then
-    echo "I will need root privelegies. \nType your \033[31mmac-password and press Enter.\033[0m"
-fi
-    SPCTLisDisabled=$(sudo spctl --status)
-if [[ $SPCTLisDisabled = "assessments enabled" ]]; then
-    sudo spctl --master-disable
-fi
-
 #install components
 
 #Use ZSH
 chsh -s /bin/zsh
 # Homebrew
-xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 #iterm2
 brew install iterm2 --cask
